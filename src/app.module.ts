17f38eafab36b76c0056d38e9c63ca6776c839e8
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-08-28 11:04:28 +0500

    feat: add jwt package and create docker file from postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: +process.env.POSTGRES_OUTPUT_PORT,
-    //   username: process.env.POSTGRES_USER,
-    //   password: process.env.POSTGRES_PW,
-    //   database: process.env.POSTGRES_DB,
-    //   entities: ['dist/**/*.entity{.ts,.js}'],
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: +process.env.POSTGRES_OUTPUT_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PW,
+      database: process.env.POSTGRES_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
